2697c94ac2ec2f9fc29a47156598d824220f1faa
Vijay S Nair <nairvijays@gmail.com>
2020-06-17 20:48:44 +0530

    Added styles for desktop

---
+++ src/components/NewsArticlesListHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NewsArticlesListHeader() {
+    return (
+        <div class="news-articles-list-header">
+            <div className="article-comments">Comments</div>
+            <div className="article-vote-count">Vote Count</div>
+            <div className="article-upvote">UpVote</div>
+            <div className="article-details">News Details</div>
+        </div>
+    )
+}
