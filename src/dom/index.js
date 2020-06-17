98dda8f9297a8d92edc1b2c1bfd98e1a41e1781c
Vijay S Nair <nairvijays@gmail.com>
2020-06-17 15:39:37 +0530

    Updating references

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from "../App";
+import * as serviceWorker from "../serviceWorker";
 
 if (window.ssr) {
   // hydrate using server rendered data
